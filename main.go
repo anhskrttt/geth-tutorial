0517ca8aa4628c77c8e50d595abc14e39079717e
anhskrttt <duonanh195@gmail.com>
2022-09-21 11:09:46 -0700

    set up client

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	client, err := ethclient.Dial("https://mainnet.infura.io")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("we have a connection")
+	_ = client // we'll use this in the upcoming sections
 }
